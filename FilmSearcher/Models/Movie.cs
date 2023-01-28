1703a62a80c500ca0ce1d3c6ed8fa41641af7d6e
Andrew <shepelev.andrew.101@gmail.com>
2023-01-28 19:13:18 +0200

    Create relationship Movie with ActorMovies

--- FilmSearcher/Models/Movie.cs
+++ FilmSearcher/Models/Movie.cs
@@ -7,11 +7,14 @@ namespace FilmSearcher.Models
     {
         [Key]
         public int MovieId { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageURL { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public MovieCategory Category { get; set; }
+
+        public ActorMovie ActorMovies { get; set; }
     }
 }
