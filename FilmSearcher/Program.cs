50b19aa6c958168b3cb45a846623381fd839dfd2
Andrew <shepelev.andrew.101@gmail.com>
2023-01-29 00:50:45 +0200

    Added 'ActorService' to ServiceCollection

--- FilmSearcher/Program.cs
+++ FilmSearcher/Program.cs
@@ -1,13 +1,17 @@
 using FilmSearcher.Data;
+using FilmSearcher.Data.Services.Implementation;
+using FilmSearcher.Data.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"))
 );
+
+builder.Services.AddScoped<IActorService, ActorService>();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
