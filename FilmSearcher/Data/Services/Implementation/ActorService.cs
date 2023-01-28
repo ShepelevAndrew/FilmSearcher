53bab58544affaebb53477ae4cb70517017b89f0
Andrew <shepelev.andrew.101@gmail.com>
2023-01-29 00:36:55 +0200

    Created 'Services' folder with interf and implnt and added 'ActorService' & 'IActorService'

---
+++ FilmSearcher/Data/Services/Implementation/ActorService.cs
@@ -0,0 +1,42 @@
+﻿using FilmSearcher.Data.Services.Interfaces;
+using FilmSearcher.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmSearcher.Data.Services.Implementation
+{
+    public class ActorService : IActorService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ActorService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAll()
+        {
+            var actors = await _dbContext.Actors.ToListAsync();
+            return actors;
+        }
+
+        public Actor GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FilmSearcher/Data/Services/Interfaces/IActorService.cs
@@ -0,0 +1,13 @@
+﻿using FilmSearcher.Models;
+
+namespace FilmSearcher.Data.Services.Interfaces
+{
+    public interface IActorService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+        Actor GetById(string id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor actor);
+        void Delete(int id);
+    }
+}
