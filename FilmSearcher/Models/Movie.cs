59a885c1d7cb45566ac6ca16e386497681282161
Andrew <shepelev.andrew.101@gmail.com>
2023-01-28 18:31:28 +0200

    Add 'Movie' model

---
+++ FilmSearcher/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using FilmSearcher.Data.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmSearcher.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory Category { get; set; }
+    }
+}
