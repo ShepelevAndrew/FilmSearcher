f7cb38ea79fdc24649f871cab9e500eb14b53bee
Andrew <shepelev.andrew.101@gmail.com>
2023-01-28 19:14:26 +0200

    Fixed mistakes and codestyles

--- FilmSearcher/Models/Producer.cs
+++ FilmSearcher/Models/Producer.cs
@@ -1,11 +1,16 @@
-﻿namespace FilmSearcher.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmSearcher.Models
 {
     public class Producer
     {
+        [Key]
         public int ProducerId { get; set; }
+
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+
         public List<Movie> Movies { get; set; }
     }
 }
