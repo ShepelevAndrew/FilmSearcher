627e5c9b64cc499aa7f2d599f3707cf5dc91ab5b
Andrew <shepelev.andrew.101@gmail.com>
2023-01-28 18:48:05 +0200

    Create relationship Producer with Movie

--- FilmSearcher/Models/Producer.cs
+++ FilmSearcher/Models/Producer.cs
@@ -6,5 +6,6 @@
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+        public List<Movie> Movies { get; set; }
     }
 }
