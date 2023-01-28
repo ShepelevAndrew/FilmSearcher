a99baf3cbad58387c37b6c2edb839f8fd7259bab
Andrew <shepelev.andrew.101@gmail.com>
2023-01-29 00:39:07 +0200

    Created 'Conrollers' folder and added 'ActorConroller'

---
+++ FilmSearcher/Controllers/ActorController.cs
@@ -0,0 +1,21 @@
+﻿using FilmSearcher.Data.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmSearcher.Controllers
+{
+    public class ActorController : Controller
+    {
+        private readonly IActorService _actorService;
+
+        public ActorController(IActorService actorService)
+        {
+            _actorService = actorService;
+        }
+
+        public async Task<IActionResult> Actors()
+        {
+            var actors = await _actorService.GetAll();
+            return View(actors);
+        }
+    }
+}
