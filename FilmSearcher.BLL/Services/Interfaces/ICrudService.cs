a2d7f37ee2509ac564af4d9118c4c8fdb0aab5d8
Andrew <shepelev.andrew.101@gmail.com>
2023-02-03 20:26:33 +0200

    CRUD service

---
+++ FilmSearcher.BLL/Services/Interfaces/ICrudService.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmSearcher.BLL.Services.Interfaces
+{
+    public interface ICrudService<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+    }
+}
