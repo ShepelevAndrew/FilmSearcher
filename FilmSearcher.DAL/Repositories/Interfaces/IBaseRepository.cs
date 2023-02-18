86639af59c5a9ed6c8a72ddda0a060189823de3e
Andrew <shepelev.andrew.101@gmail.com>
2023-02-18 18:48:40 +0200

    Renamed and moved interface of service(interface of repository)

---
+++ FilmSearcher.DAL/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmSearcher.DAL.Repositories.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+    }
+}
